edc6f7b0f06d6f6ebd4bb49249fd5670c09f319a
Myles Eftos <myles@madpilot.com.au>
2014-07-22 14:48:55 +1000

    Setting up build server status display

---
+++ buildbox.js
@@ -0,0 +1,58 @@
+(function() {
+  var fetchBuildStatus = function(callback) {
+    var request = new XMLHttpRequest();
+    request.onload = function(result) {
+      callback(null, result.target.responseXML);
+    }
+    request.open("get", "/feed.xml");
+    request.send();
+  }
+
+  var humanize = function(str) {
+    str = str.replace(/-/g, ' ');
+    words = str.split(' ');
+
+    for(var i = 0; i < words.length; i++) {
+      var letters = words[i].split('');
+      var firstLetter = letters.shift();
+      words[i] = firstLetter.toUpperCase() + letters.join('');
+    }
+    
+    return words.join(' ');
+  }
+
+  var rendered = [];
+  var process = function(xml) {
+    var projects = xml.getElementsByTagName('Project');
+    var builds = document.getElementsByClassName('builds');
+
+    for(var i = 0; i < projects.length; i++) {
+      var name = projects[i].getAttribute('name');
+      var activity = projects[i].getAttribute('activity').toLowerCase();
+      var lastStatus = projects[i].getAttribute('lastBuildStatus').toLowerCase();
+
+
+      if(rendered.indexOf(name) > -1) {
+        var build = document.getElementById(name);
+        build.className = 'build ' + activity + ' ' + lastStatus;
+      } else {
+        html = builds[0].innerHTML;
+        html += '<div id="' + name + '" class="build ' + activity + ' ' + lastStatus + '">';
+        html += '<h1>' + humanize(name) + '</h1>';
+        html += '</div>';
+        builds[0].innerHTML = html;  
+        rendered.push(name);
+      }
+    }
+  }
+
+  fetchBuildStatus(function(error, xml) {
+    process(xml);
+  });
+
+  setInterval(function() {
+    fetchBuildStatus(function(error, xml) {
+      process(xml);
+    });
+  }, 5000);
+})();
